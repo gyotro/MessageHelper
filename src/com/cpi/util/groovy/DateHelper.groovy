8820f3b6c6ac2cce064ffe44b38c17d4beaab417
Giovanni D'Introno <giovanni.dintrono@gmail.com>
2020-08-25 08:25:34 +0200

    rimozione caratteri inutili per groovy

--- src/com/cpi/util/groovy/DateHelper.groovy
+++ src/com/cpi/util/groovy/DateHelper.groovy
@@ -1,81 +1,84 @@
 package com.cpi.util.groovy
 
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoField;
-import java.time.temporal.TemporalAdjusters;
-import java.util.Date;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.function.UnaryOperator;
+import java.time.*
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoField
+import java.time.temporal.TemporalAdjusters
+import java.util.Date
+import java.util.function.BiFunction
+import java.util.function.Function
+import java.util.function.Supplier
+import java.util.function.UnaryOperator
 
 class DateHelper {
 
     // per aggiungere minuti, secondi, giorni, ore, mesi ad un LocalDateTime, usare le funzioni plusHours(n) o plusMinutes(n) o plusDays(n) queste restituiscono
     // una nuova istanza, non modificano quella già creata, perché LocalDateTime è immutable
 
-    public static Supplier<String> getActualDate = LocalDate.now().toString();
-    public static Supplier<String> getActualTime = LocalTime.now().toString();
-    public static Supplier<String> getActualEpochSeconds = Instant.now().getEpochSecond() + "";
-    public static Supplier<String> getActualEpochMillis = Instant.now().toEpochMilli() + "";
-    public static Supplier<String> getActualDateTime = LocalDateTime.now().toString();
-    public static Supplier<String> getActualMonth = LocalDateTime.now().getMonth().toString();
-    public static Supplier<String> getActualDayOfYear = "" + LocalDateTime.now().getDayOfYear();
-    public static Supplier<String> getActualDayOfMonth = "" + LocalDateTime.now().get(ChronoField.DAY_OF_MONTH);
-    public static Supplier<String> getFirstDayOfNextMonth = "" + LocalDateTime.now().with(TemporalAdjusters.firstDayOfNextMonth()) ;
-    public static Supplier<String> getIsLeap = "" + LocalDate.now().isLeapYear(); // verifica se bisestile
-    public static Supplier<String> getLocalHour = LocalTime.now().getHour() + "";
-    public static Supplier<String> getLocalMinute = LocalTime.now().get(ChronoField.MINUTE_OF_DAY) + ""; //modo alternativo per prendere il minuto
-    public static Supplier<String> getLocalSecond = LocalTime.now().getSecond() + "";
-    public static BiFunction<LocalDate, LocalDate, String> diffPeriodDays = { localStartDate, localEndDate -> Period.between(localStartDate, localEndDate).getDays() + ""; }
-    public static BiFunction<LocalTime, LocalTime, String> diffDurationSeconds = { localStartTime, localEndTime -> Duration.between(localStartTime, localEndTime).getSeconds() + ""; }
-    public static BiFunction<LocalTime, LocalTime, String> diffDurationMinutes = { localStartTime, localEndTime -> Duration.between(localStartTime, localEndTime).toMinutes() + ""; }
+    public Supplier<String> getActualDate = LocalDate.now().toString()
+    public Supplier<String> getActualTime = LocalTime.now().toString()
+    public Supplier<String> getActualEpochSeconds = Instant.now().getEpochSecond() + ""
+    public Supplier<String> getActualEpochMillis = Instant.now().toEpochMilli() + ""
+    public Supplier<String> getActualDateTime = LocalDateTime.now() + ""
+    public Supplier<String> getActualMonth = LocalDateTime.now().getMonth() + ""
+    public Supplier<String> getActualDayOfYear = "" + LocalDateTime.now().getDayOfYear()
+    public Supplier<String> getActualDayOfMonth = "" + LocalDateTime.now().get(ChronoField.DAY_OF_MONTH)
+    public Supplier<String> getFirstDayOfNextMonth = "" + LocalDateTime.now().with(TemporalAdjusters.firstDayOfNextMonth()) 
+    public  Supplier<String> getIsLeap = "" + LocalDate.now().isLeapYear() // verifica se bisestile
+    public  Supplier<String> getLocalHour = LocalTime.now().getHour() + ""
+    public  Supplier<String> getLocalMinute = LocalTime.now().get(ChronoField.MINUTE_OF_DAY) + "" //modo alternativo per prendere il minuto
+    public  Supplier<String> getLocalSecond = LocalTime.now().getSecond() + ""
+    public  BiFunction<LocalDate, LocalDate, String> diffPeriodDays = { LocalDate localStartDate, LocalDate localEndDate -> Period.between(localStartDate, localEndDate).getDays() + "" }
+    public  BiFunction<LocalTime, LocalTime, String> diffDurationSeconds = { LocalTime localStartTime, LocalTime localEndTime -> Duration.between(localStartTime, localEndTime).getSeconds() + "" }
+    public  BiFunction<LocalTime, LocalTime, String> diffDurationMinutes = { LocalTime localStartTime, LocalTime localEndTime -> Duration.between(localStartTime, localEndTime).toMinutes() + "" }
     // Se volessimo ora e data attuali di una zona determinata potremmo fare così. La zona di sistema è ZoneId.systemDefault()
-    public static UnaryOperator<String> getTimeOfZoneWithZoneId = { zoneId -> ZonedDateTime.now(ZoneId.of(zoneId)) + "";}
-    public static UnaryOperator<String> getSimpleDateTimeOfZone = { zoneId -> LocalDateTime.now(ZoneId.of(zoneId)) + "";}
-    public static Function<Long, Date> convertMillisToDate = { millis -> Date.from( Instant.ofEpochMilli(millis) ); }
+    public  UnaryOperator<String> getTimeOfZoneWithZoneId = { String zoneId -> ZonedDateTime.now(ZoneId.of(zoneId)) + ""}
+    public  UnaryOperator<String> getSimpleDateTimeOfZone = { String zoneId -> LocalDateTime.now(ZoneId.of(zoneId)) + ""}
+    public  Function<Long, Date> convertMillisToDate = { millis -> Date.from( Instant.ofEpochMilli(millis) ) }
     // from Millisecond to SQL Timestamp
-    public static Function<Long, Date> convertMillisToSQLDate = { millis -> new java.sql.Timestamp(millis); }
-    public static Function<java.sql.Timestamp, Long> getEpochFromSQLDate = { sqlDate -> sqlDate.toInstant().toEpochMilli(); }
+    public  Function<Long, Date> convertMillisToSQLDate = { millis -> new java.sql.Timestamp(millis) }
+    public  Function<java.sql.Timestamp, Long> getEpochFromSQLDate = { sqlDate -> sqlDate.toInstant().toEpochMilli() }
     // API per formatter - Parser
-    public static String parseLocalDate(String sDate, String sPatternIn, String sPatternOut)
+    public  String parseLocalDate(String sDate, String sPatternIn, String sPatternOut)
     {
         /*
          * Custom date formatter
          */
-        DateTimeFormatter dateTimeFormatterIn = DateTimeFormatter.ofPattern(sPatternIn);
-        DateTimeFormatter dateTimeFormatterOut = DateTimeFormatter.ofPattern(sPatternOut);
-        LocalDate local = LocalDate.parse(sDate, dateTimeFormatterIn);
-        return local.format(dateTimeFormatterOut);
+        DateTimeFormatter dateTimeFormatterIn = DateTimeFormatter.ofPattern(sPatternIn)
+        DateTimeFormatter dateTimeFormatterOut = DateTimeFormatter.ofPattern(sPatternOut)
+        LocalDate local = LocalDate.parse(sDate, dateTimeFormatterIn)
+        return local.format(dateTimeFormatterOut)
     }
 
-    public static String parseLocalTime(String sDate, String sPatternIn, String sPatternOut)
+    public  String parseLocalTime(String sDate, String sPatternIn, String sPatternOut)
     {
         /*
          * Custom time formatter
          */
-        DateTimeFormatter dateTimeFormatterIn = DateTimeFormatter.ofPattern(sPatternIn);
-        DateTimeFormatter dateTimeFormatterOut = DateTimeFormatter.ofPattern(sPatternOut);
-        LocalTime local = LocalTime.parse(sDate, dateTimeFormatterIn);
-        return local.format(dateTimeFormatterOut);
+        DateTimeFormatter dateTimeFormatterIn = DateTimeFormatter.ofPattern(sPatternIn)
+        DateTimeFormatter dateTimeFormatterOut = DateTimeFormatter.ofPattern(sPatternOut)
+        LocalTime local = LocalTime.parse(sDate, dateTimeFormatterIn)
+        return local.format(dateTimeFormatterOut)
     }
 
-    public static String parseLocalDateTime(String sDate, String sPatternIn, String sPatternOut)
+    public  String parseLocalDateTime(String sDate, String sPatternIn, String sPatternOut)
     {
         /*
          * Custom dateTime formatter
          */
-        DateTimeFormatter dateTimeFormatterIn = DateTimeFormatter.ofPattern(sPatternIn);
-        DateTimeFormatter dateTimeFormatterOut = DateTimeFormatter.ofPattern(sPatternOut);
-        LocalDateTime local = LocalDateTime.parse(sDate, dateTimeFormatterIn);
-        return local.format(dateTimeFormatterOut);
+        DateTimeFormatter dateTimeFormatterIn = DateTimeFormatter.ofPattern(sPatternIn)
+        DateTimeFormatter dateTimeFormatterOut = DateTimeFormatter.ofPattern(sPatternOut)
+        LocalDateTime local = LocalDateTime.parse(sDate, dateTimeFormatterIn)
+        return local.format(dateTimeFormatterOut)
     }
-
-
-
-    public static String getActualDateAndTime()
+    
+    public String getActualDateAndTime()
     {
-        return getActualDateTime.get();
+        return getActualDateTime.get()
+    }
+    public String sGetActualTimeExtended() // formato Aug 03, 2020 07:04:13 PM
+    {
+        DateTimeFormatter dateTimeFormatterOut = DateTimeFormatter.ofPattern( "MMM dd, yyyy hh:mm:ss a", Locale.ENGLISH )
+        return LocalDateTime.now().format(dateTimeFormatterOut)
     }
 }
--- src/com/cpi/util/groovy/MessageHelper.groovy
+++ src/com/cpi/util/groovy/MessageHelper.groovy
@@ -3,30 +3,51 @@ package com.cpi.util.groovy
 import com.sap.gateway.ip.core.customdev.util.Message
 import com.sap.it.api.msglog.MessageLogFactory
 
-class MessageHelper {
+class MessageHelper
+{
+    Message msg
+    MessageHelper(){}
+    MessageHelper(Message msg)
+    {
+        this.msg = msg
+    }
 
-        Message msg
-        MessageLogFactory logFactory
+    MessageLogFactory logFactory
 
-        String getHeaderValue(String headerName, String defaultValue) {
-            def headerValue = this.msg.getHeaders()[headerName]
-            if(headerValue)
-                headerValue
-            else if(defaultValue)
-                defaultValue
-            else
-                throw new RuntimeException("Header $headerName do not exist")
+    String getHeaderValue(String headerName, String defaultValue) {
+        def headerValue = this.msg.getHeaders()[headerName]
+        if(headerValue)
+            headerValue
+        else if(defaultValue)
+            defaultValue
+        else
+            throw new RuntimeException("Header $headerName do not exist")
+    }
+
+    String getHeaderValue(String headerName) {
+        getHeaderValue(headerName, null)
+    }
+
+    String getParamValue(String paramName) {
+        getParamValue(paramName, null)
+    }
+
+    String getParamValue(String paramName, String defaultValue) {
+        def paramValue = this.msg.getProperties()[paramName]
+        if(paramValue)
+            paramValue
+        else if(defaultValue)
+            defaultValue
+        else
+            throw new RuntimeException("Header $paramValue do not exist")
+    }
+
+
+    void addAttachmentAsString(String logName, String content)
+    {
+        def messageLog = this.logFactory.getMessageLog(this.msg)
+        if (messageLog) {
+            messageLog.addAttachmentAsString(logName, content, "text/plain")
         }
-
-        String getHeaderValue(String headerName) {
-            getHeaderValue(headerName, null)
-        }
-
-            void logToMPL(String logName, String content) {
-            def messageLog = this.logFactory.getMessageLog(this.msg)
-            if (messageLog) {
-                messageLog.addAttachmentAsString(logName, content, "text/plain")
-            }
-        }
-
+    }
 }
